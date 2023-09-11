45a5a9a7e2bb6774b188e05aa089499b61bdf1a9
MirkoR89 <mirko.rinelli@gmail.com>
2023-09-11 12:23:03 +0200

    fix: set api key in env file

--- src/redux/thunks/fetchData.js
+++ src/redux/thunks/fetchData.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const fetchData = createAsyncThunk('api/fetchData', async (params) => {
-    const apiKey = 'f4f50cabdb55351cc662ce1ae9cbc7bc'
+    const apiKey = process.env.API_KEY
 
     try {
         const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?${params}&appid=${apiKey}`);
