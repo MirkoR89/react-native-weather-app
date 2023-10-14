7c76bc93772e681bbf4297348c449d243b6b5c62
MirkoR89 <mirko.rinelli@gmail.com>
2023-10-14 13:54:57 +0200

    feat: animation slide bottom bar

--- src/components/layout/BottomTab.js
+++ src/components/layout/BottomTab.js
@@ -1,9 +1,14 @@
-import { StyleSheet, TouchableNativeFeedback, View } from 'react-native';
+import { useState } from 'react';
+import { Animated, Dimensions, StyleSheet, TouchableNativeFeedback, View } from 'react-native';
 import { Shadow } from 'react-native-shadow-2';
 import Icon from 'react-native-vector-icons/AntDesign';
 
 const BottomTab = ({ state, navigation, active, setActive }) => {
 
+
+    const totalWidth = Dimensions.get("window").width - 40
+    const tabWidth = (totalWidth / (state.routes.length - 1))
+    const [translateValue] = useState(new Animated.Value(0));
     const icons = [
         {
             id: 0,
@@ -23,53 +28,67 @@ const BottomTab = ({ state, navigation, active, setActive }) => {
     ]
 
     return (
-        <Shadow
-            distance={10}
-            startColor="rgba(0, 0, 0, 0.17)"
-            distanceInfluence={0.5}
-            offset={[20, 30]}
-        >
-            <View style={styles.container}>
-                {
-                    state.routes.map((route, index) => {
+        <>
+            <Animated.View
+                style={[
+                    styles.slider,
+                    {
+                        transform: [{ translateX: translateValue }],
+                        width: tabWidth,
+                    },
+                ]}
+            />
+            <Shadow
+                distance={10}
+                startColor="rgba(0, 0, 0, 0.17)"
+                distanceInfluence={0.5}
+                offset={[30, 30]}
+            >
+                <View style={{ ...styles.container, width: totalWidth }}>
+                    {
+                        state.routes.map((route, index) => {
 
-                        if (route.name === 'WeatherDetails') return
+                            if (route.name === 'WeatherDetails') return
 
-                        const onPress = () => {
-                            const event = navigation.emit({
-                                type: 'tabPress',
-                                target: route.key,
-                                canPreventDefault: true,
-                            })
+                            const onPress = () => {
+                                const event = navigation.emit({
+                                    type: 'tabPress',
+                                    target: route.key,
+                                    canPreventDefault: true,
+                                })
 
-                            const isFocused = state.index === index
+                                const isFocused = state.index === index
 
-                            if (!isFocused && !event.defaultPrevented) {
-                                navigation.navigate({ name: route.name, merge: true })
-                                setActive(route.name)
+                                if (!isFocused && !event.defaultPrevented) {
+                                    navigation.navigate({ name: route.name, merge: true })
+                                    setActive(route.name)
+                                }
+
+                                Animated.spring(translateValue, {
+                                    toValue: index * tabWidth,
+                                    velocity: 10,
+                                    useNativeDriver: true,
+                                }).start();
                             }
-                        }
-                        return (
-                            <TouchableNativeFeedback
-                                background={TouchableNativeFeedback.Ripple('#d1d5db', true, 65)}
-                                key={route.key}
-                                onPress={onPress}
-                            >
-                                <View style={{
-                                    ...styles.button,
-                                    borderBottomWidth: route.name === active ? 3 : 0,
-                                    borderBottomColor: route.name === active ? '#01175F' : '#FFF'
-                                }}>
-                                    <View style={styles.icon}>
-                                        <Icon name={icons.find(icon => icon.route === route.name).name} size={30} color={route.name === active ? '#01175F' : '#464C64'} />
-                                    </View>
-                                </View>
+                            return (
+                                <TouchableNativeFeedback
+                                    background={TouchableNativeFeedback.Ripple('#d1d5db', true, 65)}
+                                    key={route.key}
+                                    onPress={onPress}
+                                >
 
-                            </TouchableNativeFeedback>
-                        );
-                    })}
-            </View>
-        </Shadow>
+                                    <View style={styles.button}>
+                                        <View style={styles.icon}>
+                                            <Icon name={icons.find(icon => icon.route === route.name).name} size={30} color={route.name === active ? '#01175F' : '#464C64'} />
+                                        </View>
+                                    </View>
+
+                                </TouchableNativeFeedback>
+                            );
+                        })}
+                </View>
+            </Shadow>
+        </>
     );
 }
 
@@ -81,9 +100,18 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
         borderRadius: 25,
         backgroundColor: '#FFF',
-        width: 374,
         height: 78,
+        borderRadius: 10,
         margin: 20,
+    },
+    slider: {
+        height: 3,
+        zIndex: 10,
+        position: "absolute",
+        bottom: 20,
+        left: 20,
+        backgroundColor: '#01175F',
+        borderRadius: 10,
     },
     button: {
         width: 71,
