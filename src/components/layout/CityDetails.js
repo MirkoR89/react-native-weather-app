fe145214f9a6f3d3dbba0b3d36d550cc936acb99
MirkoR89 <mirko.rinelli@gmail.com>
2023-10-15 18:28:56 +0200

    feat: input searchbar with suggestions list of cities as city details component / redirection to weather details on selcetion of city

---
+++ src/components/layout/CityDetails.js
@@ -0,0 +1,74 @@
+import { useNavigation } from "@react-navigation/native";
+import { Dimensions, StyleSheet, Text, TouchableNativeFeedback, View } from "react-native";
+import Icon from "react-native-vector-icons/AntDesign";
+import { useDispatch } from "react-redux";
+import { fetchDetails } from "../../redux/thunks/fetchData";
+import { getBgAndIcon, getDateTime, kelvinToCelsius } from "../../utils/functions";
+
+const CityDetails = ({ item, i, lengthList }) => {
+
+    const dispatch = useDispatch()
+    const navigation = useNavigation()
+
+
+    const hanldeAddCity = async () => {
+        const _data = await dispatch((fetchDetails(item.coord)))
+        const data = _data.payload.current
+
+        const bgGradient = getBgAndIcon(data.weather.icon).bgGradient
+        const icon = getBgAndIcon(data.weather.icon).icon
+
+        const card = {
+            name: item.name,
+            temp: kelvinToCelsius(data.temp).toString(),
+            day: getDateTime(data.sunrise, data.timezone_offset).day,
+            date: getDateTime(data.sunrise, data.timezone_offset).date,
+            weather: data.weather[0]
+        }
+
+        navigation.navigate('WeatherDetails', { card, bgGradient, icon })
+    }
+
+    return (
+        <TouchableNativeFeedback
+            background={TouchableNativeFeedback.Ripple('#d1d5db')}
+            onPress={hanldeAddCity}
+        >
+            <View
+
+                style={{
+                    ...styles.container,
+                    borderBottomWidth: i !== lengthList ? 1 : 0,
+                    borderBottomLeftRadius: i === lengthList ? 10 : 0,
+                    borderBottomRightRadius: i === lengthList ? 10 : 0,
+                }}
+            >
+                <View >
+                    <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 16 }}> {item.name},</Text>
+                    <Text style={{ fontFamily: 'Poppins-Light', fontSize: 14 }}>{`${item.state || 'not available'} - ${item.country || 'not available'}`}</Text>
+                </View>
+                <Icon name="pluscircle" size={25} color="#01175F" />
+            </View>
+        </TouchableNativeFeedback>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#FFF",
+        width: Dimensions.get("window").width - 40,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderBottomColor: "#E5E5E5"
+    },
+    content: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    }
+})
+
+export default CityDetails
